d008705b3cf3ac86f8301b7c01762a65bd272495
Ricardo Silva <ricardosilva777@gmail.com>
2021-02-15 00:06:31 +0000

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+
+import PackageDescription
+
+let package = Package(
+  name: "CalendarControl",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
+  ],
+  products: [
+    .library(name: "CalendarControl", targets: ["CalendarControl"]),
+  ],
+  targets: [
+    .binaryTarget(name: "CalendarControl", path: "./Sources/CalendarControl.xcframework")
+  ]
+)
